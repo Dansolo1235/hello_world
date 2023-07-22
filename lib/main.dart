2cc13b27eacc189f138cf6b4436fe7a966d707a1
Dansolo1235 <danielberihun92@gmail.com>
2023-07-21 21:33:19 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+        home: Material(
+          child: Center(
+              child:Text("Hello wormnmnmld",
+                textAlign: TextAlign.center,
+                textDirection: TextDirection.rtl,
+                textScaleFactor: 4.0,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  decoration: TextDecoration.underline,
+                  backgroundColor: Colors.blue,
+                  letterSpacing: 10.0,
+                  wordSpacing:100.0 ,
+                ),
+              )
+          ),
+
+        )
+    );
+  }
+}
